don't update torch.solve() otherwise the computation is wrong

lib/csrc/ransac_voting/ransac_voting_gpu.py
@@ -104,6 +104,7 @@ def b_inv(b_mat):
     eye = b_mat.new_ones(b_mat.size(-1)).diag().expand_as(b_mat)
     try:
         b_inv, _ = torch.solve(eye, b_mat)
+        # b_inv = torch.linalg.solve(eye, b_mat)
     except:
         b_inv = eye
     return b_inv
@@ -139,7 +140,8 @@ def ransac_voting_layer_v3(mask, vertex, round_hyp_num, inlier_thresh=0.999, con
 
         coords = torch.nonzero(cur_mask).float()  # [tn,2]
         coords = coords[:, [1, 0]]
-        direct = vertex[bi].masked_select(torch.unsqueeze(torch.unsqueeze(cur_mask, 2), 3))  # [tn,vn,2]
+        # direct = vertex[bi].masked_select(torch.unsqueeze(torch.unsqueeze(cur_mask, 2), 3))  # [tn,vn,2]
+        direct = vertex[bi].masked_select(torch.unsqueeze(torch.unsqueeze(cur_mask, 2), 3).bool())  # [tn,vn,2]
         direct = direct.view([coords.shape[0], vn, 2])
         tn = coords.shape[0]
         idxs = torch.zeros([round_hyp_num, vn, 2], dtype=torch.int32, device=mask.device).random_(0, direct.shape[0])