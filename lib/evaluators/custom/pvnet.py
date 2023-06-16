2edfa96d3be2a9d74135852226ffc850360e0264
D-YF <daiyf1028@gmail.com>
2023-06-16 17:55:25 -0500

    modified customized ADD metric and tranlsation error metric

--- lib/evaluators/custom/pvnet.py
+++ lib/evaluators/custom/pvnet.py
@@ -11,7 +11,7 @@ if cfg.test.icp:
 from PIL import Image
 from lib.utils.img_utils import read_depth
 from scipy import spatial
-
+from scipy.spatial.transform import Rotation
 
 class Evaluator:
 
@@ -31,7 +31,24 @@ class Evaluator:
         self.icp_add = []
         self.cmd5 = []
         self.mask_ap = []
+        # self.euler_err = [] # degree
+        self.trans_err = [] # meter
         self.icp_render = icp_utils.SynRenderer(cfg.cls_type) if cfg.test.icp else None
+
+    def average_error(self, pose_pred, pose_gt):
+        # This line is to make sure the Z Value is positive(object points towards the camera)
+        if(pose_pred[2,3]<0):
+            pose_pred *= -1
+        translation_error = np.abs(pose_pred[:, 3] - pose_gt[:, 3])
+
+        # rotation_pred = Rotation.from_matrix(pose_pred[:, :3])
+        # rotation_gt = Rotation.from_matrix(pose_gt[:, :3])
+        # euler_error = rotation_pred.inv() * rotation_gt
+        # euler_error = euler_error.as_euler('zyx', degrees=True)
+        # euler_error = np.abs(euler_error)
+
+        self.trans_err.append(translation_error)
+        # self.euler_err.append(euler_error)
 
     def projection_2d(self, pose_pred, pose_targets, K, threshold=5):
         model_2d_pred = pvnet_pose_utils.project(self.model, K, pose_pred)
@@ -40,8 +57,14 @@ class Evaluator:
 
         self.proj2d.append(proj_mean_diff < threshold)
 
-    def add_metric(self, pose_pred, pose_targets, icp=False, syn=False, percentage=0.1):
-        diameter = self.diameter * percentage
+    def add_metric(self, pose_pred, pose_targets, icp=False, syn=False, percentage=1):
+        # diameter = self.diameter * percentage
+        diameter = 10 / 1000
+
+        # This line is to make sure the Z Value is positive(object points towards the camera)
+        if(pose_pred[2,3]<0):
+            pose_pred *= -1
+
         model_pred = np.dot(self.model, pose_pred[:, :3].T) + pose_pred[:, 3]
         model_targets = np.dot(self.model, pose_targets[:, :3].T) + pose_targets[:, 3]
 
@@ -51,13 +74,15 @@ class Evaluator:
             mean_dist = np.mean(mean_dist)
         else:
             mean_dist = np.mean(np.linalg.norm(model_pred - model_targets, axis=-1))
-
         if icp:
             self.icp_add.append(mean_dist < diameter)
         else:
             self.add.append(mean_dist < diameter)
 
     def cm_degree_5_metric(self, pose_pred, pose_targets):
+        # This line is to make sure the Z Value is positive(object points towards the camera)
+        if(pose_pred[2,3]<0):
+            pose_pred *= -1
         translation_distance = np.linalg.norm(pose_pred[:, 3] - pose_targets[:, 3]) * 100
         rotation_diff = np.dot(pose_pred[:, :3], pose_targets[:, :3].T)
         trace = np.trace(rotation_diff)
@@ -70,7 +95,7 @@ class Evaluator:
         mask_pred = torch.argmax(output['seg'], dim=1)[0].detach().cpu().numpy()
         mask_gt = batch['mask'][0].detach().cpu().numpy()
         iou = (mask_pred & mask_gt).sum() / (mask_pred | mask_gt).sum()
-        self.mask_ap.append(iou > 0.7)
+        self.mask_ap.append(iou > 0.9)
 
     def icp_refine(self, pose_pred, anno, output, K):
         depth = read_depth(anno['depth_path'])
@@ -105,18 +130,31 @@ class Evaluator:
             self.add_metric(pose_pred, pose_gt)
         self.cm_degree_5_metric(pose_pred, pose_gt)
         self.mask_iou(output, batch)
+        self.average_error(pose_pred, pose_gt)
 
     def summarize(self):
         proj2d = np.mean(self.proj2d)
         add = np.mean(self.add)
         cmd5 = np.mean(self.cmd5)
         ap = np.mean(self.mask_ap)
-        print('2d projections metric: {}'.format(proj2d))
-        print('ADD metric: {}'.format(add))
-        print('5 cm 5 degree metric: {}'.format(cmd5))
-        print('mask ap70: {}'.format(ap))
+        trans_err = np.mean(self.trans_err, axis=0)
+
+        print('2d projections metric: {:.3f}'.format(proj2d))
+        print('ADD metric: {:.3f}'.format(add))
+        print('5 cm 5 degree metric: {:.3f}'.format(cmd5))
+        print('mask ap90: {:.3f}'.format(ap))
+
+        print('Translation Error (X-axis): {:.1f} mm'.format(trans_err[0] * 1000))
+        print('Translation Error (Y-axis): {:.1f} mm'.format(trans_err[1] * 1000))
+        print('Translation Error (Z-axis): {:.1f} mm'.format(trans_err[2] * 1000))
+
+        # euler_err = np.mean(self.euler_err, axis=0)
+        # print('Euler Angle Error (X-axis): {:.1f} deg'.format(euler_err[0]))
+        # print('Euler Angle Error (Y-axis): {:.1f} deg'.format(euler_err[1]))
+        # print('Euler Angle Error (Z-axis): {:.1f} deg'.format(euler_err[2]))
+
         if self.icp_render is not None:
-            print('ADD metric after icp: {}'.format(np.mean(self.icp_add)))
+            print('ADD metric after icp: {:.3f}'.format(np.mean(self.icp_add)))
         self.proj2d = []
         self.add = []
         self.cmd5 = []
