4ecf53a31b450a38f33777d98bd21c4997c55594
D-YF <daiyf1028@gmail.com>
2023-07-11 20:20:05 -0500

    specify distplay port

--- docker/start_container.bash
+++ docker/start_container.bash
@@ -1,3 +1,8 @@
 #!/usr/bin/bash
 container_name=${1:-pvnet}
-docker start $container_name && docker exec -it $container_name bash
+xhost +local:root # Allow connections to X server
+docker start $container_name && docker exec -it \
+  -e DISPLAY=$DISPLAY \
+  --privileged \
+  $container_name bash
+xhost -local:root # Reset X server access permissions
