db4810c3003d983999fef75759eef9cf70b683dd
D-YF <daiyf1028@gmail.com>
2023-07-18 15:18:49 -0500

    corrected drawing of axes

--- lib/visualizers/fit/pvnet.py
+++ lib/visualizers/fit/pvnet.py
@@ -33,6 +33,7 @@ class Visualizer:
             pose_pred *= -1
         # shift predicted location to Ground True
         # pose_pred[:,3] = pose_gt[:,3]
+        self.compute_dif(pose_pred, pose_gt)
 
         corner_3d = np.array(anno['corner_3d'])
         corner_2d_gt = pvnet_pose_utils.project(corner_3d, K, pose_gt)
@@ -52,12 +53,41 @@ class Visualizer:
         ax.set_ylim(center_y - crop_size, center_y + crop_size)
         ################################################
 
+
+        # Calculate center of bounding box
+        center_x = np.mean(corner_2d_pred[:, 0])
+        center_y = np.mean(corner_2d_pred[:, 1])
+        shift_x = center_x - corner_2d_pred[6, 0]
+        shift_y = center_y - corner_2d_pred[6, 1]
+        # Plot X-axis
+        ax.plot([center_x , corner_2d_pred[2, 0]+shift_x], [center_y, corner_2d_pred[2, 1]+shift_y], color='r', linewidth=1)
+        ax.plot([center_x , corner_2d_gt[2, 0]+shift_x], [center_y, corner_2d_gt[2, 1]+shift_y], color='r', linewidth=1)
+        # Plot Y-axis
+        ax.plot([center_x, corner_2d_pred[4, 0]+shift_x], [center_y, corner_2d_pred[4, 1]+shift_y], color='g', linewidth=1)
+        ax.plot([center_x, corner_2d_gt[4, 0]+shift_x], [center_y, corner_2d_gt[4, 1]+shift_y], color='g', linewidth=1)
+        # Plot Z-axis
+        ax.plot([center_x, corner_2d_pred[7, 0]+shift_x], [center_y, corner_2d_pred[7, 1]+shift_y], color='b', linewidth=1)
+        ax.plot([center_x, corner_2d_gt[7, 0]+shift_x], [center_y, corner_2d_gt[7, 1]+shift_y], color='b', linewidth=1)
+
         ax.add_patch(patches.Polygon(xy=corner_2d_gt[[0, 1, 3, 2, 0, 4, 6, 2]], fill=False, linewidth=1, edgecolor='g'))
         ax.add_patch(patches.Polygon(xy=corner_2d_gt[[5, 4, 6, 7, 5, 1, 3, 7]], fill=False, linewidth=1, edgecolor='g'))
         ax.add_patch(patches.Polygon(xy=corner_2d_pred[[0, 1, 3, 2, 0, 4, 6, 2]], fill=False, linewidth=1, edgecolor='b'))
         ax.add_patch(patches.Polygon(xy=corner_2d_pred[[5, 4, 6, 7, 5, 1, 3, 7]], fill=False, linewidth=1, edgecolor='b'))
         plt.show()
         return fig
+
+    def compute_dif(self, pose_pred, pose_targets):
+        translation_distance = (pose_pred[:, 3] - pose_targets[:, 3]) * 1000 # mm
+        rotation_diff = np.dot(pose_pred[:, :3], pose_targets[:, :3].T)
+        trace = np.trace(rotation_diff)
+        trace = trace if trace <= 3 else 3
+        trace = trace if trace >= -1 else -1
+        angular_distance = np.rad2deg(np.arccos((trace - 1.) / 2.))
+        print("predicted pose: {}".format(pose_pred))
+        print("GT pose: {}".format(pose_targets))
+        print("Rotation diff matrix {}".format(rotation_diff))
+        print("Translation error: {} mm".format(translation_distance))
+        print("Angular error: {} deg".format(angular_distance))
 
     def visualize_output(self, input_img, output, batch_example):
         # output: 
@@ -120,14 +150,14 @@ class Visualizer:
         # Calculate center of bounding box
         center_x = np.mean(corner_2d_pred[:, 0])
         center_y = np.mean(corner_2d_pred[:, 1])
-        shift_x = center_x - corner_2d_pred[3, 0]
-        shift_y = center_y - corner_2d_pred[3, 1]
+        shift_x = center_x - corner_2d_pred[6, 0]
+        shift_y = center_y - corner_2d_pred[6, 1]
         # Plot X-axis
-        ax.plot([center_x , corner_2d_pred[7, 0]+shift_x], [center_y, corner_2d_pred[7, 1]+shift_y], color='r', linewidth=1)
+        ax.plot([center_x , corner_2d_pred[2, 0]+shift_x], [center_y, corner_2d_pred[2, 1]+shift_y], color='r', linewidth=1)
         # Plot Y-axis
-        ax.plot([center_x, corner_2d_pred[1, 0]+shift_x], [center_y, corner_2d_pred[1, 1]+shift_y], color='g', linewidth=1)
+        ax.plot([center_x, corner_2d_pred[4, 0]+shift_x], [center_y, corner_2d_pred[4, 1]+shift_y], color='g', linewidth=1)
         # Plot Z-axis
-        ax.plot([center_x, corner_2d_pred[2, 0]+shift_x], [center_y, corner_2d_pred[2, 1]+shift_y], color='b', linewidth=1)
+        ax.plot([center_x, corner_2d_pred[7, 0]+shift_x], [center_y, corner_2d_pred[7, 1]+shift_y], color='b', linewidth=1)
         # Add patches for corner_2d_gt and corner_2d_pred
         ax.add_patch(patches.Polygon(xy=corner_2d_pred[[0, 1, 3, 2, 0, 4, 6, 2]], fill=False, linewidth=1, edgecolor='b'))
         ax.add_patch(patches.Polygon(xy=corner_2d_pred[[5, 4, 6, 7, 5, 1, 3, 7]], fill=False, linewidth=1, edgecolor='b'))
