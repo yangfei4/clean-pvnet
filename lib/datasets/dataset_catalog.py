b197b6cf07b0afb0d4c6ccfe136a1da436467e32
D-YF <daiyf1028@gmail.com>
2023-12-13 15:51:33 -0600

    removed redundency

--- lib/datasets/dataset_catalog.py
+++ lib/datasets/dataset_catalog.py
@@ -9,18 +9,6 @@ class DatasetCatalog(object):
 
     def _create_dataset_attrs(self):
         return {
-        'LinemodTrain': {
-            'id': 'linemod',
-            'data_root': 'data/linemod/{}/JPEGImages'.format(self.cfg.cls_type),
-            'ann_file': 'data/linemod/{}/train.json'.format(self.cfg.cls_type),
-            'split': 'train'
-        },
-        'LinemodTest': {
-            'id': 'linemod',
-            'data_root': 'data/linemod/{}/JPEGImages'.format(self.cfg.cls_type),
-            'ann_file': 'data/linemod/{}/test.json'.format(self.cfg.cls_type),
-            'split': 'test'
-        },
         'FitTrain': {
             'id': 'fit',
             'data_root': 'data/{}_train'.format(self.cfg.cls_type),
@@ -31,62 +19,6 @@ class DatasetCatalog(object):
             'id': 'fit',
             'data_root': 'data/{}_test'.format(self.cfg.cls_type),
             'ann_file': 'data/{}_test/train.json'.format(self.cfg.cls_type),
-            'split': 'test'
-        },
-        'InsertMoldTrain': {
-            'id': 'custom',
-            'data_root': 'data/insert_mold_train',
-            'ann_file': 'data/insert_mold_train/train.json',
-            'split': 'train'
-        },
-        'InsertMoldTest': {
-            'id': 'custom',
-            'data_root': 'data/insert_mold_test',
-            'ann_file': 'data/insert_mold_test/train.json',
-            # 'data_root': 'data/custom',
-            # 'ann_file': 'data/custom/train.json',
-            'split': 'test'
-        },
-        'MainshellTrain': {
-            'id': 'custom',
-            'data_root': 'data/mainshell_train',
-            'ann_file': 'data/mainshell_train/train.json',
-            'split': 'train'
-        },
-        'MainshellTest': {
-            'id': 'custom',
-            'data_root': 'data/mainshell_test',
-            'ann_file': 'data/mainshell_test/train.json',
-            # 'data_root': 'data/custom',
-            # 'ann_file': 'data/custom/train.json',
-            'split': 'test'
-        },
-        'TopshellTrain': {
-            'id': 'custom',
-            'data_root': 'data/topshell_train',
-            'ann_file': 'data/topshell_train/train.json',
-            'split': 'train'
-        },
-        'TopshellTest': {
-            'id': 'custom',
-            'data_root': 'data/topshell_test',
-            'ann_file': 'data/topshell_test/train.json',
-            # 'data_root': 'data/custom',
-            # 'ann_file': 'data/custom/train.json',
-            'split': 'test'
-        },
-	    'CustomTrain': {
-            'id': 'custom',
-            'data_root': 'data/custom',
-            'ann_file': 'data/custom/train.json',
-            'split': 'train'
-        },
-        'CustomTest': {
-            'id': 'custom',
-            'data_root': 'data/custom_test',
-            'ann_file': 'data/custom_test/train.json',
-            # 'data_root': 'data/custom',
-            # 'ann_file': 'data/custom/train.json',
             'split': 'test'
         }
     }
