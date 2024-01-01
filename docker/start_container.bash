4c252583c4d8f687a55efc464012727a7b8a304f
Hammania689 <hameedabdulrashid@gmail.com>
2024-01-01 17:43:28 -0600

    Update to xhost to use the docker user group

--- docker/start_container.bash
+++ docker/start_container.bash
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 container_name=${1:-cobot_vision}
-xhost +local:root # Allow connections to X server
+xhost +local:docker 
 docker start $container_name && docker exec -it \
   -e DISPLAY=$DISPLAY \
   --privileged \
   $container_name bash
-xhost -local:root # Reset X server access permissions
