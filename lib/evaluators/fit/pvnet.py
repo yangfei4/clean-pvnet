d55bf5959ba1858730f0f67d83018a5a5b825631
D-YF <daiyf1028@gmail.com>
2024-02-02 13:56:42 -0600

    include translation and rotation error in returned summary

--- lib/evaluators/fit/pvnet.py
+++ lib/evaluators/fit/pvnet.py
@@ -202,4 +202,4 @@ class Evaluator:
         self.cmd5 = []
         self.mask_ap = []
         self.icp_add = []
-        return {'proj2d': proj2d, 'add': add, 'cmd5': cmd5, 'ap': ap}
+        return {'proj2d': proj2d, 'add': add, 'cmd5': cmd5, 'ap': ap, 'x_err_mm': trans_err[0], 'y_err_mm': trans_err[1], 'z_err_mm': trans_err[2], 'angular_err': angular_rotation}
