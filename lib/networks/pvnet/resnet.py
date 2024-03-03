01ca36e937942b67477f412c1996a3f170e63640
Hammania689 <hameedabdulrashid@gmail.com>
2024-03-03 00:32:30 -0600

    Kaiming normal weight initialization

--- lib/networks/pvnet/resnet.py
+++ lib/networks/pvnet/resnet.py
@@ -157,8 +157,7 @@ class ResNet(nn.Module):
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
+                nn.init.kaiming_normal_(m.weight, mode="fan_in", nonlinearity="relu")
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
